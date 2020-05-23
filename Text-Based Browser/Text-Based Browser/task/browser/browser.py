4d57969aee098f6bd21568327a9ced145c684c2f
Ivan Zelentsov <izelentsov@gmail.com>
2020-05-24 02:11:47 +0300

    Text-Based Browser - complete step 4

--- Text-Based Browser/Text-Based Browser/task/browser/browser.py
+++ Text-Based Browser/Text-Based Browser/task/browser/browser.py
@@ -1,41 +1,8 @@
 import sys
 import os
 import collections
+import requests
 
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
 
 # write your code here
 
@@ -49,6 +16,7 @@ def run():
     current = None
 
     while True:
+        # print(f"State: {current}; {history}")
         cmd = input()
         if cmd == 'exit':
             break
@@ -101,12 +69,13 @@ def is_url(cmd):
 
 
 def get_url(url):
-    text = None
-    if url == 'nytimes.com':
-        text = nytimes_com
-    elif url == 'bloomberg.com':
-        text = bloomberg_com
-    return text
+    url = clean_url(url)
+    r = requests.get(url)
+    return r.text
+
+
+def clean_url(url):
+    return url if url.startswith("https://") else "https://" + url
 
 
 def store(url, text, store_dir):
@@ -117,7 +86,8 @@ def store(url, text, store_dir):
 
 
 def store_name(url):
-    return url[:url.rindex('.')]
+    s = url[8:] if url.startswith("https://") else url
+    return s[:s.rindex('.')]
 
 
 run()
