655d58bff5e1e4fdeabb7e1a197138921fcba6b9
Ivan Zelentsov <izelentsov@gmail.com>
2020-05-28 23:59:29 +0300

    Smart Calculator - complete steps 4 and 5

--- Smart Calculator/Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/Smart Calculator/task/calculator/calculator.py
@@ -6,13 +6,13 @@ CMD_HELP = "help"
 
 def main():
     while True:
-        cmd, operands = read_input()
-        if cmd == CMD_EXIT:
-            break
-        if cmd == CMD_HELP:
-            print_help()
-        if len(operands) > 0:
-            print(sum(operands))
+        cmd, expr = read_input()
+        if cmd:
+            stop = do_cmd(cmd)
+            if stop:
+                break
+        if len(expr) > 0:
+            try_process(expr)
     print('Bye!')
 
 
@@ -22,12 +22,66 @@ def read_input():
         return line[1:], []
     tokens = line.split()
     if len(tokens) >= 1:
-        return None, [int(x) for x in tokens]
+        return None, tokens
     return None, []
 
 
+def do_cmd(cmd):
+    if cmd == CMD_EXIT:
+        return True
+    if cmd == CMD_HELP:
+        print_help()
+    else:
+        print("Unknown command")
+    return False
+
+
 def print_help():
-    print('The program calculates the sum of numbers')
+    print('The program calculates expressions with additions and subtractions')
+
+
+def try_process(expr):
+    try:
+        res = evaluate_expr(expr)
+    except ValueError:
+        res = None
+    finally:
+        if res is None:
+            print("Invalid expression")
+        else:
+            print(res)
+
+
+def evaluate_expr(expr):
+    buf = None
+    op = None
+    for term in expr:
+        if not buf:
+            buf = int(term)
+        elif not op:
+            op = simplify_op(term)
+            if not op:
+                return None
+        else:
+            buf = evaluate(op, buf, int(term))
+            op = None
+    return buf
+
+
+def simplify_op(op):
+    if op.startswith("+") and all(c == "+" for c in op):
+        return "+"
+    if op.startswith("-") and all(c == "-" for c in op):
+        return "-" if op.count("-") % 2 == 1 else "+"
+    return None
+
+
+def evaluate(op, a, b):
+    if op == "-":
+        return a - b
+    if op == "+":
+        return a + b
+    return None
 
 
 main()
--- Smart Calculator/Smart Calculator/task/tests.py
+++ Smart Calculator/Smart Calculator/task/tests.py
@@ -7,9 +7,13 @@ CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 
 class CalcTest(StageTest):
     def generate(self) -> List[TestCase]:
-        cases = {"100 + 200 - 300\n/exit": "0\nBye!",
-                 "4 + 6 - 8\n2 - 3 - 4\n/exit": "2\n-5\nBye!",
-                 "8\n\n-2 + 4 - 5 + 6\n9 +++ 10 -- 8\n3 --- 5\n14     -  12\n/exit": "8\n3\n27\n-2\n2\nBye!"}
+        cases = {"n = 3\nm=4\na =   5\nb = a\nn\nm\na\nb\ncount = 10\ncount\n/exit": "3\n4\n5\n5\n10\nBye!",
+                 "4 + 6 - 8\n\n\n2 - 3 - 4\n\n8 + 7 - 4\n/exit": "2\n-5\n11\nBye!",
+                 "a = 3\nb = 4\nc = 5\na + b - c\nb - c + 4 - a\na = 800\na + b + c\n/exit": "2\n0\n809\nBye!",
+                 "/command\n/exit": "Unknown command\nBye!",
+                 "a = 1\na = 2\na = 3\na\n/exit": "3\nBye!",
+                 "q\nr\nq = 10\nr = 20\nq\nr\nR\n/exit": "Unknown variable\nUnknown variable\n10\n20\nUnknown variable\nBye!",
+                 "a1 = 8\nn = a2a\na = 7 = 8\nnum = 10\n/exit": "Invalid identifier\nInvalid assignment\nInvalid assignment\nBye!"}
         return [TestCase(stdin=case,
                          attach=cases[case])
                 for case in cases]
