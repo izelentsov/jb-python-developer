1402b6c792f2475d682a2b5bac1372780c32bf3b
Ivan Zelentsov <izelentsov@gmail.com>
2020-07-19 12:17:07 +0300

    Numeric Matrix Processor - complete step 3

--- Numeric Matrix Processor/Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/processor/processor.py
@@ -1,28 +1,75 @@
+ACT_EXIT = 0
+ACT_ADD = 1
+ACT_SCALE = 2
+ACT_MULT = 3
+
+
 def main():
-    #m1 = read_matrix()
-    #m2 = read_matrix()
-    #res = msum(m1, m2)
-    #if res is not None:
-        #print_matrix(res)
-    #else:
-        #print('ERROR')
-    m = read_matrix()
+    while True:
+        action = menu()
+        if action == ACT_EXIT:
+            break
+        elif action == ACT_ADD:
+            do_addition()
+        elif action == ACT_SCALE:
+            do_scale()
+        elif action == ACT_MULT:
+            do_mult()
+        print()
+
+
+def menu():
+    print('1. Add matrices')
+    print('2. Multiply matrix by a constant')
+    print('3. Multiply matrices')
+    print('0. Exit')
+    return int(input('Your choice: '))
+
+
+def do_addition():
+    m1 = read_matrix('first')
+    m2 = read_matrix('second')
+    res = msum(m1, m2)
+    if res is not None:
+        print('The result is:')
+        print_matrix(res)
+    else:
+        print('ERROR')
+
+
+def do_scale():
+    m = read_matrix('')
     c = read_const()
     res = mscale(m, c)
+    print('The result is:')
     print_matrix(res)
 
 
-def read_matrix():
-    rows, cols = [int(s) for s in input().split()[:2]]
+def do_mult():
+    m1 = read_matrix('first')
+    m2 = read_matrix('second')
+    res = mmult(m1, m2)
+    if res is not None:
+        print('The result is:')
+        print_matrix(res)
+    else:
+        print('ERROR')
+
+
+def read_matrix(label):
+    msg = f'Enter size of {label} matrix: '
+    rows, cols = [int(s) for s in input(msg).split()[:2]]
+
+    print(f'Enter {label} matrix:')
     m = []
     for r in range(rows):
-        row = [int(n) for n in input().split()[:cols]]
+        row = [float(n) for n in input().split()[:cols]]
         m.append(row)
     return m
 
 
 def read_const():
-    return int(input())
+    return int(input('Enter constant: '))
 
 
 def msum(m1, m2):
@@ -36,6 +83,24 @@ def msum(m1, m2):
 
 def mscale(m, c):
     return [[(cell * c) for cell in row] for row in m]
+
+
+def mmult(m1, m2):
+    rows1 = len(m1)
+    cols1 = len(m1[0])
+    rows2 = len(m2)
+    cols2 = len(m2[0])
+    if cols1 != rows2:
+        return None
+
+    res = []
+    for i in range(rows1):
+        row = []
+        for j in range(cols2):
+            cell = sum([(m1[i][k] * m2[k][j]) for k in range(cols1)])
+            row.append(cell)
+        res.append(row)
+    return res
 
 
 def print_matrix(m):
