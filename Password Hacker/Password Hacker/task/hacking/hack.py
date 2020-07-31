b981ab730bf923dc1bfbf0e10feaaf1cd8d6d05f
Ivan Zelentsov <izelentsov@gmail.com>
2020-07-31 09:35:59 +0300

    Password Hacker - complete step 2

--- Password Hacker/Password Hacker/task/hacking/hack.py
+++ Password Hacker/Password Hacker/task/hacking/hack.py
@@ -1,23 +1,51 @@
 # write your code here
 import sys
 import socket
+import string
+from itertools import product, combinations
+
+CHARS = string.digits + string.ascii_lowercase
 
 
-def run(ip, port, msg):
+def run(ip, port):
+    p = brute(ip, port)
+    if p is not None:
+        print(p)
+
+
+def brute(ip, port):
     with socket.socket() as client:
         client.connect((ip, port))
-        client.send(msg.encode())
-        resp = client.recv(1024)
-    print(resp.decode())
+
+        pwds = generate_pwds()
+        for p in pwds:
+            client.send(p.encode())
+            resp = client.recv(1024).decode()
+            if resp == 'Connection success!':
+                return p
+            if resp == 'Too many attempts':
+                return None
+
+
+def generate_pwds():
+    n = 1
+    while True:
+        pwds = gen_of_len(n)
+        for p in pwds:
+            yield ''.join(p)
+        n += 1
+
+
+def gen_of_len(n):
+    return product(CHARS, repeat=n)
 
 
 def main():
     args = sys.argv
     ip = args[1]
     port = int(args[2])
-    msg = args[3]
 
-    run(ip, port, msg)
+    run(ip, port)
 
 
 main()
