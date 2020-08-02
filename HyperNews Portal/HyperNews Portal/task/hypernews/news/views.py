e291fdf8d17e09ca4bec25bb6c8589196ad22aaa
Ivan Zelentsov <izelentsov@gmail.com>
2020-08-03 00:04:34 +0300

    HyperNews Portal - complete step 1

---
+++ HyperNews Portal/HyperNews Portal/task/hypernews/news/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+# Create your views here.
+class NewsView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Coming soon")
