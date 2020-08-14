ee112543e02e6ad95cbe2da8ff90e308f7e98ace
Ivan Zelentsov <izelentsov@gmail.com>
2020-08-15 02:46:37 +0300

    Hypercar - complete step 3

--- Hypercar Service Center/Hypercar Service Center/task/hypercar/tickets/models.py
+++ Hypercar Service Center/Hypercar Service Center/task/hypercar/tickets/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+SERV_OIL = 0
+SERV_TIRES = 1
+SERV_DIAG = 2
+
+
+serv_times = {
+    SERV_OIL: 2,
+    SERV_TIRES: 5,
+    SERV_DIAG: 30
+}
+
+last_ticket_id = 0
+
+
+def new_ticket(serv_type):
+    global last_ticket_id
+    last_ticket_id += 1
+    return Ticket(last_ticket_id, serv_type)
+
+
+class Ticket:
+    def __init__(self, id, serv_type):
+        self.id = id
+        self.serv_type = serv_type
+
+
+class ServQueue:
+    def __init__(self):
+        self.q = {}
+        for s in serv_times.keys():
+            self.q[s] = []
+
+    def add(self, ticket):
+        wait_time = self.wait_time_for(ticket.serv_type)
+        self.q[ticket.serv_type].append(ticket)
+        return wait_time
+
+    def wait_time_for(self, serv_type):
+        res = 0
+        for s, t in serv_times.items():
+            res += len(self.q[s]) * t
+            if s == serv_type:
+                break
+        return res
+
+
+queue = ServQueue()
--- Hypercar Service Center/Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/Hypercar Service Center/task/hypercar/tickets/views.py
@@ -2,6 +2,7 @@ from django.views import View
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from tickets.models import new_ticket, queue, SERV_DIAG, SERV_OIL, SERV_TIRES
 
 
 class WelcomeView(View):
@@ -14,3 +15,24 @@ class MenuView(TemplateView):
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
+
+
+class GetTicketView(View):
+    def get(self, request, serv_type, *args, **kwargs):
+        serv_t = self.parse_serv_type(serv_type)
+        ticket = new_ticket(serv_t)
+        wait_time = queue.add(ticket)
+        context = {
+            't_num': ticket.id,
+            'w_time': wait_time
+        }
+        return render(request, 'tickets/get_ticket.html', context=context)
+
+    def parse_serv_type(self, serv_type_s):
+        if serv_type_s == "change_oil":
+            return SERV_OIL
+        if serv_type_s == "inflate_tires":
+            return SERV_TIRES
+        if serv_type_s == "diagnostic":
+            return SERV_DIAG
+        return None
