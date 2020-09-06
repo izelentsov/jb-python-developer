58aee6e2b93b5728089ef57ac01f3155f1cf112d
Ivan Zelentsov <izelentsov@gmail.com>
2020-09-06 22:03:54 +0300

    Regex Engine - complete steps 2, 3, 4

--- Regex Engine/Regex Engine/task/regex/regex.py
+++ Regex Engine/Regex Engine/task/regex/regex.py
@@ -6,10 +6,34 @@ def run():
 
 
 def check(re, s):
+    if (re == ''):
+        return True
+    if re[0] == '^':
+        return match(re[1:], s)
+    return find(re, s)
+
+
+def find(re, s):
+    re_len = len(re) - 1 if re[-1] == '$' else len(re)
+
+    for i in range(len(s) - re_len + 1):
+        if match(re, s[i:]):
+            return True
+    return False
+
+
+def match(re, s):
     if re == '':
         return True
+    if re == '$':
+        return s == ''
     if s == '':
         return False
+
+    return match_one(re[0], s[0]) and match(re[1:], s[1:])
+
+
+def match_one(re, s):
     if re == '.':
         return True
     return re == s
